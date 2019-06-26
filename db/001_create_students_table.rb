f714a59afed00c45a0b2ebe8d2244d24b812cabb
Berlinta <nashfft@gmail.com>
2019-06-26 03:52:00 -0700

    Done.

---
+++ db/001_create_students_table.rb
@@ -0,0 +1,10 @@
+class CreateStudentsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      
+      
+    end
+  end
+end
